451e5f1e36a0b4f428c47a32804bd2db79d8fbcc
ishaq-alphasquad <ishaq.alphasquad@gmail.com>
2022-04-25 05:47:12 +0500

    mobile responsiveness fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 //BASIC HEADING ACROSS ALL PAGES
 export const Nav = () => {
   return (
-    <nav className=" border-gray-200 ml-4 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 flex justify-center mt-10">
+    <nav className=" border-gray-200 ml-4 px-2 sm:px-4 py-2.5 rounded flex justify-center mt-10">
       <div className="container flex flex-wrap items-center justify-center mx-auto">
-        <a href="https://flowbite.com" className="flex items-center justify-center">
+        <a href="/" className="flex items-center justify-center">
           <span className="self-center lg:text-4xl text-xl font-semibold whitespace-nowrap text-gray-300">
             Art Base
           </span>
